4814f173444abfbdc8a3e597ad50c2ac0830ab38
rezaabaskhanian <39001073+rezaabaskhanian@users.noreply.github.com>
2025-11-27 14:02:07 +0330

    Delete game_app_traning_new directory

--- game_app_traning_new/internal/repository/mysql/migrations/1762350368_seed_permission_add_some_admin.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-INSERT INTO `permissions` (`id`, `title`) VALUES(1, 'user-list');
-INSERT INTO `permissions` (`id`, `title`) VALUES(2, 'user-delete');
-
-
--- +migrate Down
-DELETE FROM `permissions` WHERE id in (1,2);
